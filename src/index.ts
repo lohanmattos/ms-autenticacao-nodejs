6eff9a9cc35019f5b66bdc1f1efed34ae4ef85c2
Lohan Amendola <lohanamendola18@gmail.com>
2023-01-26 10:36:33 -0400

    Adicionando o Typescript e a lib ts-node-dev para automatizar o processo de desenvolvimento.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("ola");
+import express, {Request, Response, NextFunction} from "express";
+
+const port = 3000;
+
+const app = express();
+
+//cria uma rota get /status
+app.get('/status', (req:Request, res:Response, next:NextFunction) => {
+    res.status(200).send({ foo: 'Sucesso' });
+} )
+
+app.listen(port, ()=> {
+    console.log("Servidor rodando na porta: " + port);
+})
